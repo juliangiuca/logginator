cfaf7edea3570783b5454990c37dd64f670c4c0d
Julian Giuca <julian@newrelic.com>
2019-07-07 08:31:50 -0700

    tune

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,12 @@ module.exports = {
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     error_file: './logs/err.log',
     out_file: './logs/out.log',
-    time: true
-    instances: 5,
+    time: true,
+    instances: 4,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    exec_mode : "cluster"
     env: {
       NODE_ENV: 'development'
     },

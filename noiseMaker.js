0fc8d74e0c194fe092a5f1476183b2bbb4682416
Julian Giuca <julian@newrelic.com>
2019-07-25 21:53:48 -0700

    add logs!

--- noiseMaker.js
+++ noiseMaker.js
@@ -1,22 +1,46 @@
 const fs = require('fs');
+const gigs_an_hour = 1
+let mbs_an_hour = gigs_an_hour * 1000
 
-let book = fs.readFileSync('./alice_in_wonderland.txt', 'utf-8')
-let book_in_parts = book.split('.')
-let steps = Math.floor(book_in_parts.length / 15)
+let logs = [];
+
+fs.readdirSync('./fixtures').forEach((file) => {
+  let log = fs.readFileSync(`./fixtures/${file}`, 'utf-8')
+  let splitLogs = log.split('\n')
+  console.log(`length ${file} is ${splitLogs.length}`)
+  logs = logs.concat(splitLogs)
+})
+
+let bytes = Buffer.byteLength(logs.join('\n'), 'utf8')
+let megabytes = bytes * 1e-6;
+
+let repetition_an_hour = mbs_an_hour / megabytes
+let repetitions_a_minute = repetition_an_hour / 60
+let repetitions_a_second = repetitions_a_minute / 60
+let repetitions_200_ms = repetitions_a_second / 5
+let logs_per_interval = Math.floor(logs.length * repetitions_200_ms)
+
 let counter = 0;
 
 
 setInterval(() => {
-  let section = counter * steps;
-  for (let i=0; i<steps; i++) {
-    console.log(book_in_parts[section + i])
+  let section = counter * logs_per_interval;
+  for (let i=0; i<logs_per_interval; i++) {
+    console.log(logs[section + i])
   }
 
   counter++;
-  console.log('counter at', counter)
-
-  if (counter >= 15) {
+  if (counter * logs_per_interval >= logs.length) {
+    logs = shuffle(logs)
     counter = 0;
     console.log('starting again')
   }
-}, 1000)
+}, 200)
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}

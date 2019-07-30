29a4149406b14f0e1b4b4e0181df616b7db8ee3e
Julian Giuca <julian@newrelic.com>
2019-07-29 17:24:13 -0700

    make it simpler

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,6 @@
 module.exports = {
   apps : [
   {
-    name: 'scrape',
-    script: 'scrape.js',
-    error_file: './logs/scrape-err.log',
-    out_file: './logs/scrape-out.log',
-    time: true,
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '2G',
-    exec_mode : "cluster",
-    env: {
-      NODE_ENV: 'development',
-      PORT: 4000
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 4000
-    }
-  },
-  {
     name: 'noiseMaker',
     script: 'noiseMaker.js',
     error_file: './logs/noise-maker-err.log',
